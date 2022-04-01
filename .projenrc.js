122fab6b57b0db660d34e2dad3bc9ef828d1fafe
Marnix Dessing <m.dessing@nijmegen.nl>
2022-04-01 15:16:51 +0200

    feat: projen releases

--- .projenrc.js
+++ .projenrc.js
@@ -1,10 +1,16 @@
 const { awscdk } = require('projen');
 const { NodePackageManager } = require('projen/lib/javascript');
+const { ReleaseTrigger } = require('projen/lib/release');
 const project = new awscdk.AwsCdkTypeScriptApp({
   cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
   name: 'dns-management',
   packageManager: NodePackageManager.NPM,
+  release: true,
+  releaseTrigger: ReleaseTrigger.manual({
+    changelog: false,
+    gitPushCommand: '',
+  }),
   // devDeps: [
   //   'semantic-relsease@19.0.2',
   // ],
