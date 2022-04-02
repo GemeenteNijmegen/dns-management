2315358a0636ced3776fa4f7ec4f55c853c27ce0
Marnix Dessing <m.dessing@nijmegen.nl>
2022-04-02 12:11:59 +0200

    fix: modifyed projen release task

--- .projenrc.js
+++ .projenrc.js
@@ -9,8 +9,25 @@ const project = new awscdk.AwsCdkTypeScriptApp({
   release: true,
   releaseTrigger: ReleaseTrigger.manual({
     changelog: false,
-    gitPushCommand: '',
+    gitPushCommand: 'git push --tags',
   }),
   github: false,
 });
+
+
+// Modify the release task to not include the final git diff command as it will fail...
+const release = project.tasks.tryFind('release');
+if (release !== undefined) {
+  release.reset();
+  release.exec('rm -fr dist');
+  release.spawn(project.tasks.tryFind('bump'));
+  release.spawn(project.tasks.tryFind('build'));
+  release.spawn(project.tasks.tryFind('unbump'));
+  release.spawn(project.tasks.tryFind('publish:git'));
+  //release.exec('git diff --ignore-space-at-eol --exit-code');
+} else {
+  console.warn('No release task found in this project configuration');
+}
+
+
 project.synth();
