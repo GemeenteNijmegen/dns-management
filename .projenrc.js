4fb8aaf92b2259162ba975c1d92b6a7f51c423cb
Marnix Dessing <m.dessing@nijmegen.nl>
2022-04-12 10:07:33 +0200

    feat: automatic releases using projen (#6), license fix (#7)

--- .projenrc.js
+++ .projenrc.js
@@ -2,33 +2,20 @@ const { awscdk } = require('projen');
 const { NodePackageManager } = require('projen/lib/javascript');
 const { ReleaseTrigger } = require('projen/lib/release');
 const project = new awscdk.AwsCdkTypeScriptApp({
-  projenVersion: '0.52.72',
+  projenVersion: '0.54.14',
   cdkVersion: '2.1.0',
-  defaultReleaseBranch: 'main',
   name: 'dns-management',
+  license: 'EUPL-1.2',
   packageManager: NodePackageManager.NPM,
   release: true,
-  releaseTrigger: ReleaseTrigger.manual({
-    changelog: false,
-    gitPushCommand: 'git push --tags',
-  }),
-  github: false,
+  defaultReleaseBranch: 'production',
+  majorVersion: 1,
+  depsUpgradeOptions: {
+    workflowOptions: {
+      branches: ['acceptance'],
+    },
+  },
 });
-
-
-// Modify the release task to not include the final git diff command as it will fail...
-const release = project.tasks.tryFind('release');
-if (release !== undefined) {
-  release.reset();
-  release.exec('rm -fr dist');
-  release.spawn(project.tasks.tryFind('bump'));
-  release.spawn(project.tasks.tryFind('build'));
-  release.spawn(project.tasks.tryFind('unbump'));
-  release.spawn(project.tasks.tryFind('publish:git'));
-  //release.exec('git diff --ignore-space-at-eol --exit-code');
-} else {
-  console.warn('No release task found in this project configuration');
-}
 
 
 project.synth();
