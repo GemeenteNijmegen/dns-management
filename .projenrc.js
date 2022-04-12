87c701e4f669f1ee839b0c951adbe368fbd82e4a
Marnix Dessing <m.dessing@nijmegen.nl>
2022-04-12 10:34:39 +0200

    fix: use yarn instead of npm as package manager

--- .projenrc.js
+++ .projenrc.js
@@ -3,10 +3,9 @@ const { NodePackageManager } = require('projen/lib/javascript');
 const { ReleaseTrigger } = require('projen/lib/release');
 const project = new awscdk.AwsCdkTypeScriptApp({
   projenVersion: '0.54.14',
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.17.0',
   name: 'dns-management',
   license: 'EUPL-1.2',
-  packageManager: NodePackageManager.NPM,
   release: true,
   defaultReleaseBranch: 'production',
   majorVersion: 1,
@@ -16,6 +15,5 @@ const project = new awscdk.AwsCdkTypeScriptApp({
     },
   },
 });
-
 
 project.synth();
