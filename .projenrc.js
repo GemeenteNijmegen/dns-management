b7eafc5f6e0d491baf5d040dd84a99c573c92f53
Marnix Dessing <m.dessing@nijmegen.nl>
2022-04-13 11:17:48 +0200

    fix: clean imports

--- .projenrc.js
+++ .projenrc.js
@@ -1,6 +1,4 @@
 const { awscdk } = require('projen');
-const { NodePackageManager } = require('projen/lib/javascript');
-const { ReleaseTrigger } = require('projen/lib/release');
 const project = new awscdk.AwsCdkTypeScriptApp({
   projenVersion: '0.54.14',
   cdkVersion: '2.17.0',
