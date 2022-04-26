627baab5371ec1ad82a2bc24f412bbb9acb3424a
Marnix Dessing <m.dessing@nijmegen.nl>
2022-04-26 15:56:23 +0200

    fix: do not deploy kms key to sandbox

--- src/AccountStage.ts
+++ src/AccountStage.ts
@@ -8,17 +8,18 @@ export interface AccountStageProps extends StageProps {
   name: string;
   cspRootEnvironment: Environment;
   deployDnsStack: boolean;
+  deployDnsSecKmsKey: boolean;
 }
 
 export class AccountStage extends Stage {
   constructor(scope: Construct, id: string, props: AccountStageProps) {
     super(scope, id, props);
 
-    if (props.cspRootEnvironment.account == undefined) {
-      throw 'Account reference to csp root hosted zone account is empty, can not deploy subzones.';
-    }
-
+    // Deploy a hosted zone (sub domain of csp-nijmegen.nl)
     if (props.deployDnsStack) {
+      if (props.cspRootEnvironment.account == undefined) {
+        throw 'Account reference to csp root hosted zone account is empty, can not deploy subzones.';
+      }
       const dns = new DnsStack(this, 'stack', {
         productionAccount: props.cspRootEnvironment.account,
         rootZoneName: 'csp-nijmegen.nl',
@@ -29,11 +30,13 @@ export class AccountStage extends Stage {
     }
 
     // KMS key used for dnssec (must be in us-east-1)
-    const dnssec = new DnsSecStack(this, 'dnssec-stack', {
-      env: { region: 'us-east-1' },
-    });
+    if (props.deployDnsSecKmsKey) {
+      const dnssec = new DnsSecStack(this, 'dnssec-stack', {
+        env: { region: 'us-east-1' },
+      });
+      Aspects.of(dnssec).add(new AwsSolutionsChecks());
+    }
 
-    Aspects.of(dnssec).add(new AwsSolutionsChecks());
 
   }
 }
