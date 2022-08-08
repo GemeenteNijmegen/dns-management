053977f6eb85e23abb56e68c93b8ff0e14b317c5
Marnix Dessing <m.dessing@nijmegen.nl>
2022-08-08 12:30:13 +0200

    feat: fixed tests

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,14 +1,44 @@
+import { App, Aspects } from 'aws-cdk-lib';
+import { AwsSolutionsChecks } from 'cdk-nag';
+import { CspNijmegenStack } from '../src/CspNijmegenStack';
+import { DnsSecStack } from '../src/DnsSecStack';
+import { DnsStack } from '../src/DnsStack';
 
-test('CFN-NAG', () => {
 
-  // TODO: create stacks and enable nag
+const env = {
+  account: '123',
+  region: 'eu-west-1',
+};
 
-  // TODO: snapshot tests
+test('Snapshot', () => {
 
-  // Enable cfn-nag
-  //  Aspects.of(cspStage).add(new AwsSolutionsChecks({ verbose: true }));
-  //  Aspects.of(sandboxStage).add(new AwsSolutionsChecks({ verbose: true }));
-  //  Aspects.of(acceptanceStage).add(new AwsSolutionsChecks({ verbose: true }));
+  const app = new App();
 
+
+  const dnsstack = new DnsStack(app, 'dns-stack', {
+    productionAccount: env.account,
+    registerInCspNijmegenRoot: true,
+    rootZoneName: 'csp-nijmegen.nl',
+    subzoneName: 'dnsstack',
+  });
+
+  const dnssecstack = new DnsSecStack(app, 'dnssec-stack', {
+
+  });
+
+  const cspStack = new CspNijmegenStack(app, 'csp-stack', {
+    env: env,
+    sandbox: env,
+  });
+
+  // Nag
+  Aspects.of(dnsstack).add(new AwsSolutionsChecks({ verbose: true }));
+  Aspects.of(dnssecstack).add(new AwsSolutionsChecks({ verbose: true }));
+  Aspects.of(cspStack).add(new AwsSolutionsChecks({ verbose: true }));
+
+  // Snapshot
+  expect(app.synth().getStackArtifact(dnsstack.artifactId).template).toMatchSnapshot();
+  expect(app.synth().getStackArtifact(dnssecstack.artifactId).template).toMatchSnapshot();
+  expect(app.synth().getStackArtifact(cspStack.artifactId).template).toMatchSnapshot();
 
 });
