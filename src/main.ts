ecef818653f040dea32a83d06b330829470f097d
Marnix Dessing <m.dessing@nijmegen.nl>
2022-12-28 10:51:50 +0100

    feat: centralized dns configuration

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { App } from 'aws-cdk-lib';
+import { DnsConfiguration } from './DnsConfiguration';
 import { PipelineStack } from './PipelineStack';
 import { Statics } from './Statics';
-
 
 const app = new App();
 
 new PipelineStack(app, 'dns-management-pipeline', {
   env: Statics.deploymentEnvironment,
   branchName: 'production',
+  dnsConfiguration: DnsConfiguration,
 });
 
 app.synth();
