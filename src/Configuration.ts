4817354f229ec3672387ff4a2bb61f959af61f3c
Wieteke <w.kremer@nijmegen.nl>
2025-02-12 10:34:24 +0100

    chore: cname  open-forms-accp-haal-centraal.csp-nijmegen.nl

--- src/Configuration.ts
+++ src/Configuration.ts
@@ -78,9 +78,8 @@ export const configurations: { [key: string]: Configuration } = {
     toplevelHostedzoneEnvironment: Statics.gnNetworkEnvironment,
     subdomains: DnsConfiguration,
     cnameRecords: {
-      _16CF4D27263C264AA528D30B0C189FD7: '636E74F515CBF2C561510AC2019363DE.A849E9FC566D59538484CDE17F9AD382.sectigo.com', //vwi-accp.csp-nijmegen.nl
-      _04B53C0AB10852A9297D1488CEB7BB74: '337B8E86657ECF0E49B878E5D708237B.C869CF1543BABDE44034DCB374ADC35C.sectigo.com', //vwi-prod.csp-nijmegen.nl
-    },
+      _8EB90A827CA30DF0D15CB819DEE19249: '8A412BC57C15888A1B5C21A9B751C49A.199F432B2A40913816D5A06328BC2384.sectigo.com', // open-forms-accp-haal-centraal.csp-nijmegen.nl
+   },
   },
 };
 
