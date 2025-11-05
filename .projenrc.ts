b548a676a181703be3d331c4a91d99e2ffad9932
WietekeNijmegen <wietekenijmegen@gmail.com>
2025-11-05 13:59:42 +0100

    feat: projen project type update - ts and own lib cdk projen type

---
+++ .projenrc.ts
@@ -0,0 +1,34 @@
+import { GemeenteNijmegenCdkApp } from '@gemeentenijmegen/projen-project-type';
+const project = new GemeenteNijmegenCdkApp({
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
+  devDeps: ['@gemeentenijmegen/projen-project-type'],
+  name: 'dns-management',
+  projenrcTs: true,
+  depsUpgradeOptions: {
+    workflowOptions: {
+      branches: ['main'],
+    },
+  },
+  deps: [
+    'dotenv',
+    'cdk-nag@^2.0.0',
+    'cdk-remote-stack',
+    '@gemeentenijmegen/aws-constructs',
+    '@gemeentenijmegen/dnssec-record',
+  ],
+  gitignore: [
+    'test-reports/junit.xml',
+    'test/__snapshots__/*',
+    '.env',
+    '.vscode',
+    '.DS_Store',
+  ],
+  jestOptions: {
+    jestConfig: {
+      setupFiles: ['dotenv/config'],
+    },
+  },
+  enableAutoMergeDependencies: false, // No acceptance branch
+});
+project.synth();
